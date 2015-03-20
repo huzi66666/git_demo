45656313c22b005cac975a27e57c6280f3da5529
huzi66666 <huweijun_2012@163.com>
2015-03-20 10:15:33 +0800

    测试使用githup

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * This file created at 2015-3-20.
+ *
+ * Copyright (c) 2002-2015 Bingosoft, Inc. All rights reserved.
+ */
+package test;
+
+/**
+ * <code>{@link HelloWorld}</code>
+ *
+ * TODO : document me
+ *
+ * @author Administrator
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 */
+	private void syso() {
+		// TODO implement HelloWorld.syso
+
+	}
+}
